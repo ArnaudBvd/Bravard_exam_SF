c7eae0729402b0bc3fff79ea525ec7c816229e9a
Arnaud Bravard <bravard.arnaud@gmail.com>
2023-07-28 23:09:19 +0200

    Résolution de l'erreur 'the http status code is not a redirect (200 given)'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -43,7 +43,7 @@ class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_OK);
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/new.html.twig', [
