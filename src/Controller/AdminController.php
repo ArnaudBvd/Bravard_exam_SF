95f4668d6df9135e1ccdc0fe1e1a59ed5c1228b2
Arnaud Bravard <bravard.arnaud@gmail.com>
2023-07-30 12:44:43 +0200

    Mise en forme de la page espace administration

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
 
 class AdminController extends AbstractController
 {
@@ -13,6 +15,14 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/admin', name: 'app_admin', methods: ['GET'])]
+    public function allUsers(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'users' => $userRepository->findAll(),
         ]);
     }
 }
